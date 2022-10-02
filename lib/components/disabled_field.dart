7cd508ceeeeb64c3d0d907bc47dd36df5fc4d96f
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-02 20:59:06 +0300

    Create disabled_field.dart

---
+++ lib/components/disabled_field.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DisabledField extends StatelessWidget {
+  const DisabledField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
