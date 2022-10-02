cb8ddf760104d3a92c1ecdcb150d7d078583a0f8
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-02 20:59:11 +0300

    Create email_field.dart

---
+++ lib/components/email_field.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EmailField extends StatelessWidget {
+  const EmailField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
