ed77940b7a222d86398739c2827b356c14c6fe32
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-02 20:59:19 +0300

    Create number_field.dart

---
+++ lib/components/number_field.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NumberField extends StatefulWidget {
+  const NumberField({Key? key}) : super(key: key);
+
+  @override
+  State<NumberField> createState() => _NumberFieldState();
+}
+
+class _NumberFieldState extends State<NumberField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
