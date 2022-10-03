3e01590b957ea1287cf8658104932c91598d796c
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-03 09:37:04 +0300

    Update text_field_row.dart

--- lib/components/text_field_row.dart
+++ lib/components/text_field_row.dart
@@ -4,9 +4,17 @@ class TextFieldRow extends StatelessWidget {
   const TextFieldRow({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-
-    );
-  }
+  Widget build(BuildContext context) => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(
+                helperText: "", hintText: "Row", labelText: "Row 1"),
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+                helperText: "", hintText: "Row", labelText: "Row 2"),
+          ),
+        ],
+      );
 }
