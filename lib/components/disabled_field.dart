0cfb23fb6b0d4aaa4230a567fe6fa8c5b6283643
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-03 09:27:59 +0300

    Update disabled_field.dart

--- lib/components/disabled_field.dart
+++ lib/components/disabled_field.dart
@@ -4,9 +4,12 @@ class DisabledField extends StatelessWidget {
   const DisabledField({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-
-    );
-  }
+  Widget build(BuildContext context) => TextFormField(
+    decoration: const InputDecoration(
+      enabled: false,
+      labelText: "Disabled",
+      helperText: "",
+      hintText: "Disabled"
+    ),
+  );
 }
