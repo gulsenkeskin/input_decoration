437647f2fc3e97f6b4cc99c5199c8ecd9546ec56
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-03 09:31:25 +0300

    Update multiline_field.dart

--- lib/components/multiline_field.dart
+++ lib/components/multiline_field.dart
@@ -4,9 +4,20 @@ class MultilineField extends StatelessWidget {
   const MultilineField({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-
-    );
-  }
+  Widget build(BuildContext context) => TextFormField(
+        maxLines: 5,
+        decoration: InputDecoration(
+            helperText: "",
+            hintText: "Multiline",
+            label: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Icon(Icons.info_outline),
+                Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Text("Multiline"),
+                )
+              ],
+            )),
+      );
 }
