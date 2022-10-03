f0311644c417f1c204357cae97ccecdf4914ec58
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-03 09:24:51 +0300

    Update email_field.dart

--- lib/components/email_field.dart
+++ lib/components/email_field.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:input_decoration/string_extensions.dart';
 
 class EmailField extends StatelessWidget {
   const EmailField({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-
-    );
-  }
+  Widget build(BuildContext context) => TextFormField(
+    keyboardType: TextInputType.emailAddress,
+    validator: (value){
+      if(!value!.isValidEmail()){
+        return 'Lütfen geçerli bir email adresi girin.';
+      }
+    },
+    decoration: const InputDecoration(
+      labelText: "Email",
+      helperText: "",
+      hintText: "email@gmail.com"
+    ),
+  );
 }
